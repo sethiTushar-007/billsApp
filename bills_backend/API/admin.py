e09cf7fdc3cf05d3d184ecf7c93d7683bc62c4ba
Tushar Sethi <sethitushar2000@gmail.com>
2021-03-24 13:11:23 +0530

    added sites admin

--- bills_backend/API/admin.py
+++ bills_backend/API/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-
+from django.contrib.sites.model import SiteAdmin
 # Register your models here.
 
 @admin.register(Item)
@@ -18,3 +18,12 @@ class BillAdmin(admin.ModelAdmin):
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'no', 'name', 'email')
+
+admin.site.unregister(Site)
+class SiteAdmin(admin.ModelAdmin):
+    fields = ('id', 'name', 'domain')
+    readonly_fields = ('id',)
+    list_display = ('id', 'name', 'domain')
+    list_display_links = ('name',)
+    search_fields = ('name', 'domain')
+admin.site.register(Site, SiteAdmin)
